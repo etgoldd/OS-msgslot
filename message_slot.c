0565f3db64e491c39c6fdbf3cfe28b5b79c79457
etgoldd <etgold05@gmail.com>
2024-02-22 00:57:25 +0200

    Finished Driver.

--- message_slot.c
+++ message_slot.c
@@ -8,6 +8,7 @@
 #define MODULE
 
 
+
 #include <linux/kernel.h>   /* We're doing kernel work */
 #include <linux/module.h>   /* Specifically, a module */
 #include <linux/fs.h>       /* for register_chrdev */
@@ -19,13 +20,16 @@ MODULE_LICENSE("GPL");
 //Our custom definitions of IOCTL operations
 #include "message_slot.h"
 
+// Declarations for self.
+access_ok( buffer, length );
+
+
 // used to prevent concurent access into the same device
 static int dev_open_flag = 0;
 
-static struct chardev_info device_info;
-
 // The message the device will give when asked
-static char the_message[BUF_LEN];
+static unsigned int buffer_lengths[256];
+static char message_buffers[256][BUF_LEN];
 
 //Do we need to encrypt?
 static int encryption_flag = 0;
@@ -34,18 +38,9 @@ static int encryption_flag = 0;
 static int device_open( struct inode* inode,
                         struct file*  file )
 {
-  unsigned long flags; // for spinlock
   printk("Invoking device_open(%p)\n", file);
+  file->private_data = iminor(inode);
 
-  // We don't want to talk to two processes at the same time
-  spin_lock_irqsave(&device_info.lock, flags);
-  if( 1 == dev_open_flag ) {
-    spin_unlock_irqrestore(&device_info.lock, flags);
-    return -EBUSY;
-  }
-
-  ++dev_open_flag;
-  spin_unlock_irqrestore(&device_info.lock, flags);
   return SUCCESS;
 }
 
@@ -53,13 +48,7 @@ static int device_open( struct inode* inode,
 static int device_release( struct inode* inode,
                            struct file*  file)
 {
-  unsigned long flags; // for spinlock
-  printk("Invoking device_release(%p,%p)\n", inode, file);
-
-  // ready for our next caller
-  spin_lock_irqsave(&device_info.lock, flags);
-  --dev_open_flag;
-  spin_unlock_irqrestore(&device_info.lock, flags);
+  file->private_data = NULL;
   return SUCCESS;
 }
 
@@ -71,12 +60,33 @@ static ssize_t device_read( struct file* file,
                             size_t       length,
                             loff_t*      offset )
 {
-  // read doesnt really do anything (for now)
-  printk( "Invocing device_read(%p,%ld) - "
-          "operation not supported yet\n"
-          "(last written - %s)\n",
-          file, length, the_message );
-  //invalid argument error
+  // Checking if a channel has been set.
+  if (file->private_data == NULL) {
+    return -EINVAL;
+  }
+
+  // Checking if the relevant buffer has a message
+  unsigned int index = (int)file->private_data;
+  if (buffer_lengths[index] == 0) {
+    return -EWOULDBLOCK;
+  }
+
+  // Validating that the buffer is long enough for the message.
+  if (length < buffer_lengths[index]) {
+    return -ENOSPC;
+  }
+
+  // Checking buffer validity.
+  if( buffer == NULL ) {
+    return -EINVAL;
+  }
+  if ( !access_ok( buffer, length ) ) {
+    return -EFAULT;
+  }
+  // Copying the message to the buffer.
+  if( copy_to_user( buffer, message_buffers[index], length ) == 0 ) {
+    return length;
+  }
   return -EINVAL;
 }
 
@@ -88,16 +98,22 @@ static ssize_t device_write( struct file*       file,
                              size_t             length,
                              loff_t*            offset)
 {
-  ssize_t i;
-  printk("Invoking device_write(%p,%ld)\n", file, length);
-  for( i = 0; i < length && i < BUF_LEN; ++i ) {
-    get_user(the_message[i], &buffer[i]);
-    if( 1 == encryption_flag )
-      the_message[i] += 1;
+  // Checking if a channel has been set
+  if (file->private_data == NULL) {
+    return -EINVAL;
   }
- 
-  // return the number of input characters used
-  return i;
+  int index = (int)file->private_data;
+  if (length > BUF_LEN || length == 0) {
+    return -EMSGSIZE;
+  }
+  // Checking the buffer validity
+  if (buffer == NULL) {
+    return -EINVAL;
+  }
+  if (access_ok(buffer)) {
+    return -EFAULT;
+  }
+  return (buffer_lengths[index] = copy_from_user(message_buffers[(int)file->private_data], buffer, length)));
 }
 
 //----------------------------------------------------------------
@@ -105,14 +121,16 @@ static long device_ioctl( struct   file* file,
                           unsigned int   ioctl_command_id,
                           unsigned long  ioctl_param )
 {
-  // Switch according to the ioctl called
-  if( IOCTL_SET_ENC == ioctl_command_id ) {
-    // Get the parameter given to ioctl by the process
-    printk( "Invoking ioctl: setting encryption "
-            "flag to %ld\n", ioctl_param );
-    encryption_flag = ioctl_param;
-  }
 
+  // Switch according to the ioctl called
+  if(IOCTL_SET_CHNL != ioctl_command_id ) {
+    return SUCCESS;
+    // Get the parameter given to ioctl by the process
+  }
+  if (ioctl_param > 255) {
+    return -EINVAL;
+  }
+  file->private_data = ioctl_param;
   return SUCCESS;
 }
 
@@ -134,9 +152,6 @@ struct file_operations Fops = {
 static int __init simple_init(void)
 {
   int rc = -1;
-  // init dev struct
-  memset( &device_info, 0, sizeof(struct chardev_info) );
-  spin_lock_init( &device_info.lock );
 
   // Register driver capabilities. Obtain major num
   rc = register_chrdev( MAJOR_NUM, DEVICE_RANGE_NAME, &Fops );
@@ -147,14 +162,6 @@ static int __init simple_init(void)
                        DEVICE_FILE_NAME, MAJOR_NUM );
     return rc;
   }
-
-  printk( "Registeration is successful. ");
-  printk( "If you want to talk to the device driver,\n" );
-  printk( "you have to create a device file:\n" );
-  printk( "mknod /dev/%s c %d 0\n", DEVICE_FILE_NAME, MAJOR_NUM );
-  printk( "You can echo/cat to/from the device file.\n" );
-  printk( "Dont forget to rm the device file and "
-          "rmmod when you're done\n" );
 
   return 0;
 }
--- message_slot.h
+++ message_slot.h
@@ -1,8 +1,21 @@
-#ifndef HW3_MESSAGE_SLOT_H
-#define HW3_MESSAGE_SLOT_H
+#ifndef CHARDEV_H
+#define CHARDEV_H
 
-#include <asm/uaccess.h>
+#include <linux/ioctl.h>
 
+// The major device number.
+// We don't rely on dynamic registration
+// any more. We want ioctls to know this
+// number at compile time.
+//#define MAJOR_NUM 244
+#define MAJOR_NUM 255
 
+// Set the message of the device driver
+#define IOCTL_SET_CHNL _IOW(MAJOR_NUM, 0, unsigned long)
 
-#endif //HW3_MESSAGE_SLOT_H
+#define DEVICE_RANGE_NAME "char_dev"
+#define BUF_LEN 128
+#define DEVICE_FILE_NAME "simple_char_dev"
+#define SUCCESS 0
+
+#endif
