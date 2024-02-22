40ad569132a921e70b1c1faa7aafbec47a6016ce
etgoldd <etgold05@gmail.com>
2024-02-22 15:59:14 +0200

    implemented message_sender

---
+++ message_sender.c
@@ -0,0 +1,31 @@
+#include "message_slot.h"
+
+#include <fcntl.h>	/* open */
+#include <unistd.h>     /* exit */
+#include <sys/ioctl.h>  /* ioctl */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+  int file_desc;
+  int ret_val;
+  if (argc != 4) {
+    printf("Usage: %s <device file> <channel id> <message>\n", argv[0]);
+    exit(-1);
+  }
+
+  file_desc = open( "/dev/"DEVICE_FILE_NAME, O_RDWR );
+  if( file_desc < 0 ) {
+    printf ("Can't open device file: %s\n", DEVICE_FILE_NAME);
+    exit(-1);
+  }
+
+  ret_val = ioctl(file_desc, MSG_SLOT_CHANNEL, atoi(argv[2]));
+  ret_val = write(file_desc, argv[3], strlen(argv[3]) );
+//  ret_val = remove("/dev/"DEVICE_FILE_NAME);
+
+  close(file_desc);
+  return 0;
+}
